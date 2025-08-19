b120347ec600b3b16c8f24bb07c0be5c1b9a7164
Ujjwal Basnet <basnetujjwal98@gmail.com>
2025-08-19 19:44:50 +0545

    secret key updated

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/test", (req, res) => {
-  res.status(200).send("Test running...");
+  res.status(200).send("Test running successfully...");
 });
 
 module.exports = app;
