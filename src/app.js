00ce24df35385525d6139cc24c7ec630b10b40d2
Ujjwal Basnet <basnetujjwal98@gmail.com>
2025-08-21 14:52:50 +0545

    add new api endpoint

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.status(200).send("Hello from render.com");
+});
+
 app.get("/test", (req, res) => {
   res.status(200).send("Test running...");
 });
